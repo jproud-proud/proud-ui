2fddbbe99299602a24db1598e5e9d539cd73d8d3
powerJproud <jproud1996@163.com>
2022-11-18 09:38:12 +0800

    新增button组件

---
+++ examples/views/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <pd-button
+      type="primary"
+      @click="handleClick"
+      disabled
+    >primary</pd-button>
+    <pd-button
+      type="success"
+      round
+    >success</pd-button>
+    <pd-button
+      type="warning"
+      icon="info"
+    >warning</pd-button>
+    <pd-button
+      type="danger"
+      @click="handleClick"
+    >danger</pd-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  methods: {
+    handleClick() {
+      alert(1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>

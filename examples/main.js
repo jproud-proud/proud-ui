b09fa1a41541d8b06ee61cb9a7f7b9732c6860d5
powerJproud <jproud1996@163.com>
2022-11-18 09:37:18 +0800

    修改部分文件位置

--- examples/main.js
+++ examples/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import proudUI from './packages/index' // 引入自己的ui库
+import router from './router'
+import proudUI from '../packages/components/index' // 引入自己的ui库
 Vue.use(proudUI) // 挂载到全局，在代码里就可以直接用了
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
